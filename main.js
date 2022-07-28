c3a21b82cfc6f314782fc930fce28a14837ee5f5
Inaki Merino <inakimerino2308@gmail.com>
2022-07-28 00:20:14 -0300

    Evento carrito

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 
 
 
-let deporte = (tipo, pago, capacidad, precio)
+/* let deporte = (tipo, pago, capacidad, precio) */
 
-function deporte (tipo, pago, capacidad){
-    this.tipo = tipo;
-    this.pago = pago;
-    this.capacidad = capacidad;
-    this.precio = precio;
+class Deporte {
+    constructor(tipo, pago, capacidad, precio) {
+        this.tipo = tipo;
+        this.pago = pago;
+        this.capacidad = capacidad;
+        this.precio = precio;
+    }
 }
+const padelWorld = new Deporte ("padel", "alquiler", 4, 3000);
+const siempre10 = new Deporte ("futbol", "alquiler", 22, 5000);
+const colzenneger = new Deporte ("gimnasio", "membresia", 8500);
+const crossfit = new Deporte ("gimnasio", "membresia", 30, 4600);
+const tenisRapidas = new Deporte ("tenis", "alquiler", 4, 5500);
+const palace = new Deporte ("tenis", "alquiler", 4, 6000)
 
-const padelWorld = new deporte ("padel", "alquiler", 4, 3000);
-const siempre10 = new deporte ("futbol", "alquiler", 22, 5000);
-const colzenneger = new deporte ("gimnasio", "membresia", 8500);
-const crossfit = new deporte ("gimnasio", "membresia", 30, 4600);
-const tenisRapidas = new deporte ("tenis", "alquiler", 4, 5500);
-const palace = new deporte ("tenis", "alquiler", 4, 6000)
+let productos = [padelWorld,siempre10,colzenneger,crossfit,tenisRapidas,palace]
 
 
-function carrito (){
-    let valorCarrito = 0;
-    let carrito = [];
+let valorCarrito = 0;
+let carrito = [];
+function carritoCompras (){
     const sumaCarrito = carrito.reduce((valorCarrito, carrito) => valorCarrito = carrito, 0)
 }
+
+const prodAgregado = document.querySelector(".btn")
+function agregarCarrito (){
+    prodAgregado.addEventListener("click",clickCarrito);
+    carrito.push(prodAgregado)
+    console.log(carrito)
+}
+
