7f86eb9c63959887eb28465294798999b0c58029
Inaki Merino <inakimerino2308@gmail.com>
2022-08-11 00:05:33 -0300

    Modals con librerias

--- main.js
+++ main.js
@@ -32,3 +32,64 @@ function clickCarrito (){
     valorCarrito = valorCarrito + precioProducto;
     carrito.push(prodAgregado); 
 }
+
+
+const botonColzeneger = document.querySelector("#colz")
+
+botonColzeneger.addEventListener('click', function(){
+	Swal.fire({
+        title: 'Colzenneger Gym',
+        text: 'Cuotas trimestrales de 7500 pesos. Ubicado a tres cuadras de la terminal.',
+        confirmButtonText: 'Agregar al Carrito'
+    })
+});
+
+const botonSiempre = document.querySelector("#siemp")
+
+botonSiempre.addEventListener('click', function(){
+	Swal.fire({
+        title: 'Siempre al 10',
+        text: 'LLama por telefono y consulta horarios, precio y disponibilidad del bar.',
+        confirmButtonText: 'Agregar al Carrito'
+    })
+});
+
+const botonXGym = document.querySelector("#xgym")
+
+botonXGym.addEventListener('click', function(){
+	Swal.fire({
+        title: 'X gym',
+        text: 'Elegi tu forma d epago, mensual trimestral o semestral, y consegui descuentos increibles.',
+        confirmButtonText: 'Agregar al Carrito'
+    })
+});
+
+const botonPade = document.querySelector("#pade")
+
+botonPade.addEventListener('click', function(){
+	Swal.fire({
+        title: 'Padel World',
+        text: '1200 la hora, consulta nuestro schedule de torneos y anotate en el que se ajuste a tu nivel.',
+        confirmButtonText: 'Agregar al Carrito'
+    })
+});
+
+const botonSpor = document.querySelector("#spor")
+
+botonSpor.addEventListener('click', function(){
+	Swal.fire({
+        title: 'Sport Palace',
+        text: '800 la hora, amplia cantidad de canchas, no dudes en consultar horarios, se pueden pedir horarios fijos.',
+        confirmButtonText: 'Agregar al Carrito'
+    })
+});
+
+const botonRaps = document.querySelector("#raps")
+
+botonRaps.addEventListener('click', function(){
+	Swal.fire({
+        title: 'Tenis Rapidas',
+        text: '1400 la hora, clases durante los dias de semana, profesorado de tenis y padel todas las mananas.',
+        confirmButtonText: 'Agregar al Carrito'
+    })
+});
